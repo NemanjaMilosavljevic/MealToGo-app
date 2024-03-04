88c4cd80ef1f0006e3412e8bc5782a99f72072ea
Nemanja Milosavljevic <milosavljevicnemanja1994@gmail.com>
2024-03-04 12:27:37 +0100

    added component for active links

---
+++ components/navLink.js
@@ -0,0 +1,20 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import "./navLink.css";
+
+const NavLink = ({ href, children }) => {
+  const path = usePathname();
+  return (
+    <Link
+      className={`nav-link ${path.startsWith(href) ? `activePage` : ""}`}
+      aria-current="page"
+      href={href}
+    >
+      {children}
+    </Link>
+  );
+};
+
+export default NavLink;
