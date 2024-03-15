f584732d52c367c62cf02233265783102e474aeb
NemanjaMilosavljevic <135519682+NemanjaMilosavljevic@users.noreply.github.com>
2024-03-15 17:40:44 +0100

    protect "/admin" route

--- middleware.js
+++ middleware.js
@@ -1,16 +1,25 @@
-export function middleware(request) {
-  const currentUser = request.cookies.get("next-auth.session-token")?.value;
-  console.log("middleware");
+import { getToken } from "next-auth/jwt";
 
-  if (currentUser && request.nextUrl.pathname.startsWith("/login")) {
-    return Response.redirect(new URL("/", request.url));
+export async function middleware(req) {
+  const currentUser = req.cookies.get("next-auth.session-token")?.value;
+
+  const userEmail = (await getToken({ req }))?.email;
+  const isAdmin = userEmail?.includes("@admin");
+
+  if (currentUser && req.nextUrl.pathname.startsWith("/login")) {
+    return Response.redirect(new URL("/", req.url), 302);
   }
 
   if (
-    (!currentUser && request.nextUrl.pathname.startsWith("/favorites")) ||
-    request.nextUrl.pathname.startsWith("/order")
+    !currentUser &&
+    (req.nextUrl.pathname.startsWith("/favorites") ||
+      req.nextUrl.pathname.startsWith("/order"))
   ) {
-    return Response.redirect(new URL("/login", request.url));
+    return Response.redirect(new URL("/login", req.url), 302);
+  }
+
+  if (!isAdmin && req.nextUrl.pathname.startsWith("/admin")) {
+    return Response.redirect(new URL("/", req.url), 302);
   }
 }
 
